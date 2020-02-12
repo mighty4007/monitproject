caf2e695ad11778056c5ff20eadd973ee18ca19a
mighty4007 <60222594+mighty4007@users.noreply.github.com>
2020-02-12 19:01:26 +0530

    Insert

--- PartnerDetailsController.java
+++ PartnerDetailsController.java
@@ -1,5 +1,30 @@
-	@Query(value = "SELECT * FROM partner_details u WHERE u.partner_code = ?1 and u.request_date >= ?2",nativeQuery=true)
-	//PartnerDetails getTrendAnalysisReportOne(String partnerCode);
-	//public Optional<PartnerDetails> getTrendAnalysisReportOne(String userName);
-	public List<PartnerDetails> getTrendAnalysisReportOne(String partnerCode,Date requestDate);
-	
+insert into  partner_hits (id,agent_code,no_of_hits,no_of_send_txn_hits,no_of_cancel_txn_hits,no_of_enquiry_txn_hits,no_of_rates_hits,no_of_solr_search_hits)
+values (1,'UAEXMUKRX001',12000,4530,2540,544,23,1982);
+
+insert into  partner_hits (id,agent_code,no_of_hits,no_of_send_txn_hits,no_of_cancel_txn_hits,no_of_enquiry_txn_hits,no_of_rates_hits,no_of_solr_search_hits)
+values (2,'UAEEXCARX001',9000,3000,3000,1000,1000,1000);
+
+insert into  partner_hits (id,agent_code,no_of_hits,no_of_send_txn_hits,no_of_cancel_txn_hits,no_of_enquiry_txn_hits,no_of_rates_hits,no_of_solr_search_hits)
+values (3,'UAEEXAURX001',8000,2500,1500,500,322,112);
+
+insert into  partner_hits (id,agent_code,no_of_hits,no_of_send_txn_hits,no_of_cancel_txn_hits,no_of_enquiry_txn_hits,no_of_rates_hits,no_of_solr_search_hits)
+values (4,'UAEXMUKRX002',8500,3000,1500,424,323,2323);
+
+insert into  partner_hits (id,agent_code,no_of_hits,no_of_send_txn_hits,no_of_cancel_txn_hits,no_of_enquiry_txn_hits,no_of_rates_hits,no_of_solr_search_hits)
+values (5,'UAEXMUKRX003',10500,2000,996,233,111,331);
+
+insert into  partner_hits (id,agent_code,no_of_hits,no_of_send_txn_hits,no_of_cancel_txn_hits,no_of_enquiry_txn_hits,no_of_rates_hits,no_of_solr_search_hits)
+values (6,'UAEXMUKRX008',1100,4530,3221,121,222,211);
+
+
+insert into  partner_hits (id,agent_code,no_of_hits,no_of_send_txn_hits,no_of_cancel_txn_hits,no_of_enquiry_txn_hits,no_of_rates_hits,no_of_solr_search_hits)
+values (7,'UAEEXAU00002',8700,3223,1222,123,222,113);
+
+insert into  partner_hits (id,agent_code,no_of_hits,no_of_send_txn_hits,no_of_cancel_txn_hits,no_of_enquiry_txn_hits,no_of_rates_hits,no_of_solr_search_hits)
+values (8,'UAEXMUKRX044',13000,5789,3323,133,1333,131);
+
+insert into  partner_hits (id,agent_code,no_of_hits,no_of_send_txn_hits,no_of_cancel_txn_hits,no_of_enquiry_txn_hits,no_of_rates_hits,no_of_solr_search_hits)
+values (9,'UAEEXAU00001',15000,5678,3133,1211,313,313);
+
+insert into  partner_hits (id,agent_code,no_of_hits,no_of_send_txn_hits,no_of_cancel_txn_hits,no_of_enquiry_txn_hits,no_of_rates_hits,no_of_solr_search_hits)
+values (10,'UAEXMUKRX103',11322,3323,1333,1313,313,333);
