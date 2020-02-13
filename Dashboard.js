dae0665314071aac139da6d5a203ed523dc56d90
mighty4007 <60222594+mighty4007@users.noreply.github.com>
2020-02-13 18:38:46 +0530

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -7,251 +7,126 @@ import 'react-accessible-accordion/dist/fancy-example.css';
 import { HorizontalBar } from 'react-chartjs-2';
 import Widget from "../../components/Widget";
 
-
-
-
+import ReactApexChart from 'react-apexcharts'
 
 
 export default class Dashboard extends Component {
 
-  constructor(){
-    super();
-    this.state = {
-      count:'',
-      top10:{labels:[],datasets:[]},
-      data:{data:{openPrice:[], highPrice:[], lowPrice:[], ltp:[], previousPrice:[], netPrice:[], tradedQuantity:[], turnoverInLakhs:[], lastCorpAnnouncementDate:[],lastCorpAnnouncement:[]}},
-      cardData:{open:[], high:[], low:[], last:[], previousClose:[], percChange:[], yearHigh:[], yearLow:[], indexOrder:[]},
-      cardData1:{agentCode:[], noOfHits:[], noOfSendTxnHits:[], noOfCancelTxnHits:[],noOfEnquiryTxnHits:[],noOfSolrSearchHits:[]
-               ,noOfRatesHits:[]}
-    }
+
+  state = {
+
+    series: [],
+    options: {}
+      
   }
 
-  storeData(temp){
-
-
-    //var len = Object.keys(temp).length
-    const agentCode=[];
-    const noOfHits=[];
-    const noOfSendTxnHits=[];
-    const noOfCancelTxnHits=[];
-
-    //for(var i=0; i<10; i++)
-    for (const key in temp) 
-    {
-
-
-      agentCode[key]=temp.data[key].agentCode
-      noOfSendTxnHits[key]=temp.data[key].noOfSendTxnHits
-      noOfCancelTxnHits[key]=temp.data[key].noOfCancelTxnHits
-      noOfHits[key]=temp.data[key].noOfHits
-
-
-    }
-    this.setState({
-      cardData: {
-        agentCode: agentCode,
-        noOfHits: noOfHits,
-        noOfSendTxnHits: noOfSendTxnHits,
-        noOfCancelTxnHits: noOfCancelTxnHits
-      }})
-  }
-  
-  componentWillMount(){
+  componentWillMount() {
     document.title = 'Load Monitoring'
-    this.fetchStocks();
+    this.fetchData();
   }
 
+  fetchData() {
+    console.log("in did getAllEntityData STAdsadsdsdasds")
+    Axios.defaults.baseURL = 'http://myurl';
+    Axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+    Axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
-  fetchStocks() {
-    //Axios.get('https://nseindia.com/live_market/dynaContent/live_watch/stock_watch/liveIndexWatchData.json').then(res => { 
-      Axios.defaults.baseURL = 'http://myurl';
-      Axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
-      Axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-    //Axios.get('http://localhost:8080/').then(res => { 
+    Axios.get('http://localhost:8888/getAllEntityData/').then(entity_res => {
 
-    
-      Axios.get('http://localhost:8888/getAllEntityData/').then(entity_res => {
-        
-        const entity_labels= [];
-        const entity_methods= [];
+      const entity_labels = [];
+      const entity_methods = [];
 
-        console.log("in did getAllEntityData STATE",entity_res)
+      console.log("in did getAllEntityData STATE", entity_res)
 
-        const response =entity_res.data;
-        const hitDataGroupBy_Entity =response.hitDataGroupBy_Entity;
-        const hitDataGroupBy_Entity_Method =response.hitDataGroupBy_Entity_Method;
-        const chartData =response.chartData;
-       
-
-        console.log("hitDataGroupBy_Entity:",hitDataGroupBy_Entity)
-        console.log("hitDataGroupBy_Entity_Method:",hitDataGroupBy_Entity_Method)
-        console.log("chartData:",chartData)
-
-        for (const key in hitDataGroupBy_Entity) {
-          entity_labels[key]=hitDataGroupBy_Entity[key].entityCode
-        }
-
-   
-        console.log("entity_labels:",entity_labels)
-
-        this.setState({
-          top10:{
-            labels: entity_labels,
-            datasets:chartData
-          }        
-        })
-
-        }
-      )
+      const response = entity_res.data;
+      const hitDataGroupBy_Entity = response.hitDataGroupBy_Entity;
+      const hitDataGroupBy_Entity_Method = response.hitDataGroupBy_Entity_Method;
+      const chartData = response.chartData;
 
 
-    Axios.get('http://localhost:8007/partnerHits/').then(res => { 
-      console.log("in did MUICustomDataTable STATE",res.data)
-      this.count = Object.keys(res.data).length
-      console.log("in did THIS DATA COUNT",this.count)
+      console.log("hitDataGroupBy_Entity:", hitDataGroupBy_Entity)
+      console.log("hitDataGroupBy_Entity_Method:", hitDataGroupBy_Entity_Method)
+      console.log("chartData:", chartData)
 
-      //this.data = this.dumpComma(res.data);
-      this.data = res.data;
-      //this.fcolor(this.data);
-      console.log("in did THIS DATA STATE",this.data)
-
-      
-
-     // this.storeData(this.data);
-      const labels= [];
-      const sendHits=[];
-      const cancelHits=[];
-      const noOfHits=[];
-      const enquiryHits=[];
-      const solrHits=[];
-      const ratesHits=[];
-      
-
-
-      const otherMethodHits=[];
-
-      for (const key in res.data) {
-
-        
-        labels[key]=res.data[key].agentCode
-        sendHits[key]=res.data[key].noOfSendTxnHits
-        cancelHits[key]=res.data[key].noOfCancelTxnHits
-        noOfHits[key]=res.data[key].noOfHits
-
-        enquiryHits[key]=res.data[key].noOfEnquiryTxnHits
-        solrHits[key]=res.data[key].noOfSolrSearchHits
-        ratesHits[key]=res.data[key].noOfRatesHits
+      for (const key in hitDataGroupBy_Entity) {
+        entity_labels[key] = hitDataGroupBy_Entity[key].entityCode
       }
 
-      console.log("log_Key:",labels)
-      console.log("log_Key:",sendHits)
+
+      console.log("entity_labels:", entity_labels)
 
       this.setState({
-        cardData1: {
-          agentCode: labels,
-          noOfHits: noOfHits,
-          noOfSendTxnHits: sendHits,
-          noOfCancelTxnHits: cancelHits,
-          noOfEnquiryTxnHits:enquiryHits,
-          noOfSolrSearchHits:solrHits,
-          noOfRatesHits:ratesHits
-        }})
-
-        const dataforbar =[
-          {
-            label:'Txn Hits',
-            backgroundColor:"red",
-            data: cancelHits
+       
+       
+        options: {
+          chart: {
+            type: 'bar',
+            height: 350,
+            stacked: true,
           },
-          {
-            label:'Send Txn Hits',
-            backgroundColor:"blue",
-            data: sendHits
+          plotOptions: {
+            bar: {
+              horizontal: true,
+            },
           },
-          {
-            label:'Total No of Hits',
-            backgroundColor:"#FF5733",
-            data: noOfHits
+          stroke: {
+            width: 1,
+            colors: ['#fff']
+          },
+          title: {
+            text: 'Top 10 Hitters Method Wise'
+          },
+          xaxis: {
+            categories: entity_labels,
+            labels: {
+              formatter: function (val) {
+                return val 
+              }
+            }
+          },
+          yaxis: {
+            title: {
+              text: undefined
+            },
+          },
+          tooltip: {
+            y: {
+              formatter: function (val) {
+                return val + "K"
+              }
+            }
+          },
+          fill: {
+            opacity: 1
+          },
+          legend: {
+            position: 'top',
+            horizontalAlign: 'left',
+            offsetX: 40
           }
-        ]
+        },
+        
+        series: chartData,
+       
+      })
 
-        console.log("data for bar:",dataforbar)
-
-      // this.setState({
-      //   top10:{
-      //     labels: labels,
-      //     datasets:dataforbar
-      //   }        
-      // })
     }
-
-    
-      )
-  }
-
-
-   render(){
-      var chart= this.state.top10
-      var indents = [];
-      console.log("SSSSSSSSSSSSSSSSSS",chart.labels.length);
-
-    
-      for (var i = 0; i < chart.labels.length; i++) {
-      indents.push(<Grid item lg={6} md={4} sm={6} xs={12}>
-        <Accordion allowMultipleExpanded="true" allowZeroExpanded="true">
-         <AccordionItem>
-          <AccordionItemHeading>
-              <AccordionItemButton><strong>{this.state.top10.labels[i]}</strong></AccordionItemButton>
-          </AccordionItemHeading>
-          <AccordionItemPanel>
-            <Widget>
-            
-                <Typography variant="h6"><i>No of Hits: {this.state.cardData1.noOfHits[i]}</i></Typography>
-                <Typography variant="h6"><i>Send: {this.state.cardData1.noOfSendTxnHits[i]}</i></Typography>
-                <Typography variant="h6"><i>Cancel: {this.state.cardData1.noOfCancelTxnHits[i]}</i></Typography>
-                <Typography variant="h6"><i>Enquiry: {this.state.cardData1.noOfEnquiryTxnHits[i]}</i></Typography>
-                <Typography variant="h6"><i>Solr: {this.state.cardData1.noOfSolrSearchHits[i]}</i></Typography>
-                <Typography variant="h6"><i>Rates: {this.state.cardData1.noOfRatesHits[i]}</i></Typography>
-          
-            </Widget>
-            </AccordionItemPanel>
-            </AccordionItem>
-          </Accordion>
-        </Grid>);
-      }
-    return(
-      <React.Fragment>
-        <div>
-        <Grid container spacing={32}   justify="center" alignItems="flex-start">
-        <Grid item md={10}>
-        <Widget title={<Typography variant="h3" component="h2" gutterBottom='true'>TOP 10 Hits</Typography>} >
-        <HorizontalBar
-            data={this.state.top10}
-            options={{
-              scales:{
-                yAxes: [{
-                  stacked: true,
-              }]
-              },
-                legend:{
-                display: false,
-                position: 'top'
-                },
-                onClick: function (evt,array){
-                  if(array[0]){
-                  console.log(array[0]._index)
-                  alert("You can find full details for this agent code "+chart.labels[array[0]._index]+" from the below card deck")
-                  }
-                }
-            }}/>
-            </Widget>
-            </Grid>
-            </Grid>
-            <Grid container spacing={32}>
-              {indents}
-            </Grid>
-        </div>
-    </React.Fragment>
     )
+
   }
+
+
+
+  render() {
+    return (
+      <div>
+        <div id="chart">
+          <ReactApexChart options={this.state.options} series={this.state.series} type="bar" height={350} />
+        </div>
+        {/* <div id="html-dist"></div> */}
+      </div>
+    );
+  }
+
+ 
 }
